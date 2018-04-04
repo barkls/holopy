ea5ac61a071028fb58b8fed5db2a8f8bd80426c9
Solomon Barkley <barkley@g.harvard.edu>
2018-04-04 18:09:24 -0400

    remove include from tmatrix code

--- holopy/scattering/theory/tmatrix_f/S.lp.f
+++ holopy/scattering/theory/tmatrix_f/S.lp.f
@@ -277,7 +277,10 @@ C   ANY DAMAGES THAT MAY RESULT FROM THE USE OF THE PROGRAM.
 
  
       IMPLICIT REAL*8 (A-H,O-Z)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       REAL*16 LAM,MRR,MRI,X(NPNG2),W(NPNG2),S(NPNG2),SS(NPNG2),
      *        AN(NPN1),R(NPNG2),DR(NPNG2),PPI,PIR,PII,P,EPS,A,
      *        DDR(NPNG2),DRR(NPNG2),DRI(NPNG2),ANN(NPN1,NPN1)
@@ -593,7 +596,10 @@ C   CALCULATION OF THE AMPLITUDE MATRIX
  
       SUBROUTINE AMPL (NMAX,DLAM,TL,TL1,PL,PL1,ALPHA,BETA,
      &                 VV,VH,HV,HH)  
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*8 (A-B,D-H,O-Z), COMPLEX*16 (C)
       REAL*8 AL(3,2),AL1(3,2),AP(2,3),AP1(2,3),B(3,3),
      *       R(2,2),R1(2,2),C(3,2),CA,CB,CT,CP,CTP,CPP,CT1,CP1,
@@ -879,7 +885,10 @@ C     1.LE.N.LE.NMAX
 C     0.LE.X.LE.1
 
       SUBROUTINE VIGAMPL (X, NMAX, M, DV1, DV2)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*8 (A-H,O-Z)
       REAL*8 DV1(NPN6), DV2(NPN6)
       DO 1 N=1,NMAX
@@ -962,7 +971,10 @@ C**********************************************************************
  
       SUBROUTINE CONST (NGAUSS,NMAX,MMAX,P,X,W,AN,ANN,S,SS,NP,EPS)
       IMPLICIT REAL*16 (A-H,O-Z)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       REAL*16 X(NPNG2),W(NPNG2),X1(NPNG1),W1(NPNG1),
      *        X2(NPNG1),W2(NPNG1),
      *        S(NPNG2),SS(NPNG2),
@@ -1104,7 +1116,10 @@ C**********************************************************************
  
       SUBROUTINE VARY (LAM,MRR,MRI,A,EPS,NP,NGAUSS,X,P,PPI,PIR,PII,
      *                 R,DR,DDR,DRR,DRI,NMAX)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*16 (A-H,O-Z)
       REAL*16 X(NPNG2),R(NPNG2),DR(NPNG2),MRR,MRI,LAM,
      *        Z(NPNG2),ZR(NPNG2),ZI(NPNG2),
@@ -1314,7 +1329,10 @@ C                                                                     *
 C**********************************************************************
  
       SUBROUTINE BESS (X,XR,XI,NG,NMAX,NNMAX1,NNMAX2)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*16 (A-H,O-Z)
       REAL*16 X(NG),XR(NG),XI(NG),
      *        J(NPNG2,NPN1),Y(NPNG2,NPN1),JR(NPNG2,NPN1),
@@ -1417,7 +1435,10 @@ C                                                                     *
 C**********************************************************************
  
       SUBROUTINE CJB (XR,XI,YR,YI,UR,UI,NMAX,NNMAX)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*16 (A-H,O-Z)
       REAL*16 YR(NMAX),YI(NMAX),UR(NMAX),UI(NMAX)
       REAL*16 CYR(NPN1),CYI(NPN1),CZR(1200),CZI(1200),
@@ -1499,7 +1520,10 @@ C**********************************************************************
  
       SUBROUTINE TMATR0 (NGAUSS,X,W,AN,ANN,S,SS,PPI,PIR,PII,R,DR,DDR,
      *                  DRR,DRI,NMAX,NCHECK)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*16 (A-H,O-Z)
       REAL*16 X(NPNG2),W(NPNG2),AN(NPN1),S(NPNG2),SS(NPNG2),
      *        R(NPNG2),DR(NPNG2),SIG(NPN2),
@@ -1738,7 +1762,10 @@ C**********************************************************************
  
       SUBROUTINE TMATR (M,NGAUSS,X,W,AN,ANN,S,SS,PPI,PIR,PII,R,DR,DDR,
      *                  DRR,DRI,NMAX,NCHECK)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*16 (A-H,O-Z)
       REAL*16 X(NPNG2),W(NPNG2),AN(NPN1),S(NPNG2),SS(NPNG2),
      *        R(NPNG2),DR(NPNG2),SIG(NPN2),
@@ -2021,7 +2048,10 @@ c     1.LE.N.LE.NMAX
 c     0.LE.x.LE.1
  
       SUBROUTINE VIG (X,NMAX,M,DV1,DV2)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*16 (A-H,O-Z)
       REAL*16 DV1(NPN1), DV2(NPN1)
       A=1Q0
@@ -2079,7 +2109,10 @@ C                                                                     *
 C**********************************************************************
  
       SUBROUTINE TT(NMAX,NCHECK)
-      INCLUDE 'amplq.par.f'
+      PARAMETER (NPN1=200, NPNG1=600, NPNG2=2*NPNG1, NPN2=2*NPN1,  
+     &           NPL=NPN2+1, NPN3=NPN1+1,  
+     &           NPN4=NPN1, NPN5=2*NPN4, NPN6=NPN4+1)
+C   changed from      INCLUDE 'amplq.par.f'
       IMPLICIT REAL*8 (A-H,O-Z)
       REAL*16 F(NPN2,NPN2),B(NPN2),WORK(NPN2),COND,
      *       QR(NPN2,NPN2),QI(NPN2,NPN2),
